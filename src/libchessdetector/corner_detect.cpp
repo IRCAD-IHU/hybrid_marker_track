f0f8f8414d688b16717e09700174478cd2dd9ed6
Maxime Guinin <mguinin@gmail.com>
2019-07-16 14:47:13 +0200

    chore(corner_detect): remove too much indentation

--- src/libchessdetector/corner_detect.cpp
+++ src/libchessdetector/corner_detect.cpp
@@ -267,7 +267,7 @@
 void corner_detect5(const size_t w, const size_t h, const uint8_t image[], int16_t response[])
 {
     // funny bounds due to sampling ring radius (5) and border of previously applied blur (2)
-    #pragma omp parallel for schedule(auto)
+    #pragma omp parallel for schedule(dynamic)
     for(int y = 7; y < h - 7; y++)
         for(int x = 7; x < w - 7; x++) {
             const unsigned offset = x + y * w;
@@ -297,41 +297,41 @@ void corner_detect5(const size_t w, const size_t h, const uint8_t image[], int16
             uint16_t diff_response = 0;
             uint16_t mean = 0;
 
-	        uint8_t a = circular_sample[0];
-	        uint8_t b = circular_sample[4];
-	        uint8_t c = circular_sample[8];
-	        uint8_t d = circular_sample[12];
+	    uint8_t a = circular_sample[0];
+	    uint8_t b = circular_sample[4];
+	    uint8_t c = circular_sample[8];
+	    uint8_t d = circular_sample[12];
 
-	        sum_response += abs(a - b + c - d);
-	        diff_response += abs(a - c) + abs(b - d);
-	        mean += a + b + c + d;
+	    sum_response += abs(a - b + c - d);
+	    diff_response += abs(a - c) + abs(b - d);
+	    mean += a + b + c + d;
 
-	        a = circular_sample[1];
-	        b = circular_sample[5];
-	        c = circular_sample[9];
-	        d = circular_sample[13];
+	    a = circular_sample[1];
+	    b = circular_sample[5];
+	    c = circular_sample[9];
+	    d = circular_sample[13];
 
-	        sum_response += abs(a - b + c - d);
-	        diff_response += abs(a - c) + abs(b - d);
-	        mean += a + b + c + d;
+	    sum_response += abs(a - b + c - d);
+	    diff_response += abs(a - c) + abs(b - d);
+	    mean += a + b + c + d;
 
-	        a = circular_sample[2];
-	        b = circular_sample[6];
-	        c = circular_sample[10];
-	        d = circular_sample[14];
+	    a = circular_sample[2];
+	    b = circular_sample[6];
+	    c = circular_sample[10];
+	    d = circular_sample[14];
 
-	        sum_response += abs(a - b + c - d);
-	        diff_response += abs(a - c) + abs(b - d);
-	        mean += a + b + c + d;
+	    sum_response += abs(a - b + c - d);
+	    diff_response += abs(a - c) + abs(b - d);
+	    mean += a + b + c + d;
 
-	        a = circular_sample[3];
-	        b = circular_sample[7];
-	        c = circular_sample[11];
-	        d = circular_sample[15];
+	    a = circular_sample[3];
+	    b = circular_sample[7];
+	    c = circular_sample[11];
+	    d = circular_sample[15];
 
-	        sum_response += abs(a - b + c - d);
-	        diff_response += abs(a - c) + abs(b - d);
-	        mean += a + b + c + d;
+	    sum_response += abs(a - b + c - d);
+	    diff_response += abs(a - c) + abs(b - d);
+	    mean += a + b + c + d;
 
             response[offset] = sum_response - diff_response - abs(mean - local_mean);
         }
